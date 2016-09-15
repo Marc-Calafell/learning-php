18bd4cbb384d516a1f657bec837a6bb3ef97781a
Marc <mcalafellsmx@gmail.com>
2016-09-15 21:58:36 +0200

    m

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title>asd</title>
-</head>
-<body>
-    <h1>
-        <?= 'hola,'. htmlspecialchars($_GET['nom']);?>
-    </h1>
-</body>
-</html>
+<?php
+
+$hola="hola, $_GET['nom']";
+
+require 'index.template.php';
---
+++ tempate.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>asd</title>
+</head>
+<body>
+    <h1>
+        <?= $hola;?>
+    </h1>
+</body>
+</html>
