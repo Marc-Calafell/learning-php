713ef2e333837fc5431645c5e0e546fbd2775db9
Marc <mcalafellsmx@gmail.com>
2016-09-15 21:22:27 +0200

    m

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php
-
-$hola="Hello";
-$name="Marc";
-echo $hola.", ".$name."\n";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1><?php echo "hola mon"; ?></h1>
+</body>
+</html>
