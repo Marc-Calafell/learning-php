16ce3b935172d8d5e3fba9bcc1868ddda85ca33f
Marc <mcalafellsmx@gmail.com>
2016-09-20 22:00:31 +0200

    examples 123

--- database/queryBuilder.php
+++ database/queryBuilder.php
@@ -6,6 +6,26 @@
  * Date: 20/09/16
  * Time: 21:33
  */
+
+class contract {
+    /**
+     * contract constructor.
+     */
+    public function __construct($arquitece, )
+    {
+    }
+
+    public function builtHome(){
+        $this->arquitectes->dissenyar();
+        $this->paleta->construir();
+        $this->electricista->enchufar();
+
+    }
+
+}
+
+
+
 class queryBuilder
 {
            //Dependencies de la classe --> dependency injection
