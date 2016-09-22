f0eb845e876589273b729112c3cee05f0e76ba80
Marc <mcalafellsmx@gmail.com>
2016-09-22 20:17:28 +0200

    Sync

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 //db config
-$config = [
+$config=[
     "type" => "mysql",
     "host" => "127.0.0.1",
     "name" => "TODO",
--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require 'tempate.php';
 require 'Task.php';
 require 'database/Connection.php';
 require 'database/queryBuilder.php';
-require 'config/database.php';
-//$config = require 'config/database.php';
+//require 'config/database.php';
+$config = require 'config/database.php';
 $message = require 'config/message.php';
 
 $pdo = connection::make($config,$message);
