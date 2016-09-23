d1cdcb7e13843f6f91172f29ffca44efc87f44a7
Marc <mcalafellsmx@gmail.com>
2016-09-23 16:48:28 +0200

    start a forms

--- core/router.php
+++ core/router.php
@@ -15,15 +15,14 @@ class router
 
     }
 
-    public function direct($uri){
-
-        if ( array_key_exists($uri,$this-> routes) ) {
-            return $this-> routes[$uri];
+    public function direct($uri)
+    {
+        if ( array_key_exists($uri, $this->routes) ) {
+            return $this->routes[$uri];
         } else {
             throw new Exception("No s'ha trobat la ruta");
         }
-
-}
+    }
 
     public static function load($routes){
         $router= new static;
